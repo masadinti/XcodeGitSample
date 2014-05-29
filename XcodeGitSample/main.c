99001937618a62c69a5ffe465faa3f2ef2ad6bd3
masato <masato@masato-no-MacBook-Air.local>
2014-05-29 11:56:02 +0900

    Initial Commit

---
+++ XcodeGitSample/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  XcodeGitSample
+//
+//  Created by masato on 2014/05/29.
+//  Copyright (c) 2014年 abcc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
